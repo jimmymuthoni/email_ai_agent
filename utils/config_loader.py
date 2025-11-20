f6dbe3c7eba2fa1abb7005b38d7210ff3bd678a8
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-11-21 01:48:47 +0300

    handling missing config.yml

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -4,7 +4,7 @@ def load_config(config_path:str = None):
     """function to get all configurations and load them"""
     if config_path is None:
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        config_path = os.path.join(BASE_DIR,"config","config.yaml")
+        config_path = os.path.join(BASE_DIR,"config","config.yml")
     
     if not os.path.exists(config_path):
         raise FileNotFoundError (f"configurationfile not found: {config_path}")
