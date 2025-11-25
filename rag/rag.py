d5bb82da6d4b6467ecb4cf9f99b6619717e79db2
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-11-25 21:35:14 +0300

    method to load vault content and perform embedding

--- rag/rag.py
+++ rag/rag.py
@@ -1,4 +1,5 @@
 import os
+import json
 from openai import OpenAI
 import torch
 from torch import Tensor
@@ -26,4 +27,23 @@ class LocalRAG:
         self.load_vault()
 
     def load_vault(self):
-        """method to load vault content"""
+        """method to load vault content,check embeddings,generate if not generate,load them, convert them to tensor"""
+        #check if the vault path exists
+        if not os.path.exists(self.config['vault_file']):
+            raise FileNotFoundError(f"vault file {self.config['vault_file']} not found.Create it first")
+        #load the text content
+        with open(self.config['vault_file'], 'r', encoding="utf-8") as file:
+            self.vault_text = file.read().split("\n")
+        #check embeddings, generate embeddings if they are not found
+        if not os.path.exists(self.config['embeddings_file']):
+            raise FileNotFoundError("Embeddings file not found.Generating embeddings....")
+        #load the embeddings
+        with open(self.config['embeddings_file']) as file:
+            self.embeddings = json.load(file)
+        #convert embeddings to tensor
+        self.embeddings_tensor = torch.tensor(self.embeddings)
+
+    
+        
+
+        
