48c15b83d8b98492e28c880bb341b3b32538e109
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-12-01 19:14:18 +0300

    module to upload different file formats and append their content to vault

--- upload.py
+++ upload.py
@@ -66,7 +66,57 @@ def upload_txtfile():
                 for chunk in chunks:
                     vault_file.write(chunk.strip() + "\n")
 
-            print("Text file contwnt appended to vault.text with each chunk on a separate line.")
+            print("Text file content appended to vault.text with each chunk on a separate line.")
+
+
+def upload_json_file():
+    """function to upload json file and aapend the content to vault.txt"""
+    file_path = filedialog.askopenfilename(filetypes=[("JSON Files", "*.json")])
+    if file_path:
+        with open(file_path,'r', encoding="utf-8") as json_file:
+            data = json.load(json_file)
+
+            #flattening json file content into  asingle string
+            text = json.dumps(data,ensure_ascii=False)
+
+            #normalize the whitespsce and clean up he text
+            text = re.sub(r'\s+','',text).strip()
+
+            sentences = re.split(r'(?<=[.!?]) +', text)
+            chunks = []
+            current_chunk = ""
+            for sentence in sentences:
+                if len(current_chunk) + len(sentence) + 1 < 1000:  # +1 for the space
+                    current_chunk += (sentence + " ").strip()
+                else:
+                    chunks.append(current_chunk)
+                    current_chunk = sentence + " "
+            if current_chunk:
+                chunks.append(current_chunk)
+            with open("vault.txt", "a", encoding="utf-8") as vault_file:
+                for chunk in chunks:
+                    vault_file.write(chunk.strip() + "\n")  # Two newlines to separate chunks
+
+            print(f"JSON file content appended to vault.txt with each chunk on separate line.")
+
+#main window
+root = tk.Tk()
+root.title("Upload ---> .pdf, .txt or .json file")
+
+#button to open the files in different format
+pdf_button = tk.Button(root,text="Upload PDF",command=convert_pdf_to_text)
+pdf_button.pack(padx=10)
+text_button = tk.Button(root,text="Upload Text File",command=upload_txtfile)
+text_button.pack(padx=10)
+json_button = tk.Button(root,text="Upload JSON",command=upload_json_file)
+json_button.pack(padx=10)
+
+#main event loop
+root.mainloop()
+
+
+
+
                 
             
                      
