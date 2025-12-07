77bce4604871e1804306d49d5902e2c005dc694f
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-12-07 13:59:20 +0300

    loading and generating embeddings

--- rag/emailrag.py
+++ rag/emailrag.py
@@ -28,4 +28,20 @@ def open_file(file_path):
         print(f"File '{file_path}' not found.")
         return None
     
+def load_or_generate_embeddings(vault_content,embeddings_file,client):
+    """this function loads embeddings if no embeddings it generate and loads"""
+    if os.path.exists(embeddings_file):
+        print(f"Loading embeddings from '{embeddings_file}'...")
+        try:
+            with open(embeddings_file, 'r', encoding="utf-8") as file:
+                return torch.tensor(json.load(file))
+        except json.JSONDecodeError:
+            print(f"Invalid JSON format in embeddings file '{embeddings_file}'.")
+            embeddings = []
+    else:
+        print("No embeddings found. Generating new embeddings")
+        embeddings = generate_embeddings(vault_content, client)
+        save_embeddings(embeddings, embeddings_file)
+    return torch.tensor(embeddings)
+    
 
